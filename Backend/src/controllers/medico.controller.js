8f3076e82f8a86b7c03c0de17e7fd5b4c03a3fd9
Rodrigo Paz <rodripaz45@gmail.com>
2024-04-04 17:11:50 -0400

    cambios med

--- Backend/src/controllers/medico.controller.js
+++ Backend/src/controllers/medico.controller.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 
 export const getMedico = async (req, res) => {
     try {
-        const { rows } = await pool.query('SELECT id, nombres, apellidos, especialidades, descripcion, username FROM medicos');
+        const { rows } = await pool.query('SELECT id, nombres, apellidos, especialidades, descripcion, username, horario_inicio, horario_fin, dias_trabajo FROM medicos');
         res.send(rows);
     } catch (error) {
         console.error('Error en la consulta SQL:', error);
@@ -13,15 +13,23 @@ export const getMedico = async (req, res) => {
 
 
 export const createMedico = async (req, res) => {
-    const { nombres, apellidos, especialidades, descripcion, username, password } = req.body;
+    const { nombres, apellidos, especialidades, descripcion, username, password, horario_inicio, horario_fin, dias_trabajo } = req.body;
+
+    // Verificar que el campo 'nombres' no sea nulo o vacío
+    if (!nombres || nombres.trim() === '') {
+        return res.status(400).send({ message: 'El campo nombres es requerido' });
+    }
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10); // Cambié el factor de hashing de 6 a 10 para mayor seguridad
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        const { rows } = await pool.query('INSERT INTO medicos (nombres, apellidos, especialidades, descripcion, username, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, nombres, apellidos, especialidades, descripcion, username', [nombres, apellidos, JSON.stringify(especialidades), descripcion, username, hashedPassword]);
-        
+        const { rows } = await pool.query(
+            'INSERT INTO medicos (nombres, apellidos, especialidades, descripcion, username, password, horario_inicio, horario_fin, dias_trabajo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id, nombres, apellidos, especialidades, descripcion, username',
+            [nombres, apellidos, JSON.stringify(especialidades), descripcion, username, hashedPassword, horario_inicio, horario_fin, dias_trabajo]
+        );
+
         const medicoToken = rows[0];
-        
+
         res.status(201).send({ message: 'Médico creado correctamente', medico: medicoToken });
     } catch (error) {
         console.error('Error al insertar en la base de datos:', error);
